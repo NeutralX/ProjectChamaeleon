956ebfeccbbec54dd0f82c3b3fddf886ee6a5e87
Alex <Alex@DESKTOP-KH2V179>
2019-05-12 23:25:22 +0200

    bossmap colliders fets i programat

--- ProjectChamaeleon/Assets/Scripts/GameController.cs
+++ ProjectChamaeleon/Assets/Scripts/GameController.cs
@@ -8,6 +8,7 @@ public class GameController : MonoBehaviour
 
     public GameObject TinyWoodsFloor1;
     public GameObject TinyWoodsFloor2;
+    public GameObject BossFloor;
     public GameObject Bulbasaur;
     public GameObject Raticate;
 
@@ -17,7 +18,9 @@ public class GameController : MonoBehaviour
     {
         TinyWoodsFloor1.SetActive(true);
         TinyWoodsFloor2.SetActive(false);
+        BossFloor.SetActive(false);
         int actualFloor = 1;
+        Bulbasaur.transform.position = new Vector3(-4f, -1.54606f, 1f);
     }
 
     // Update is called once per frame
@@ -28,16 +31,23 @@ public class GameController : MonoBehaviour
     
     public void ChangeFloor()
     {
-        TinyWoodsFloor1.SetActive(false);
-        Bulbasaur.transform.position = new Vector3(5.4f,-5.57f,1);
-        TinyWoodsFloor2.SetActive(true);
-       
+        if (TinyWoodsFloor1.activeSelf)
+        {
+            TinyWoodsFloor1.SetActive(false);
+            Bulbasaur.transform.position = new Vector3(5.4f, -5.57f, 1);
+            TinyWoodsFloor2.SetActive(true);
+        } else if (TinyWoodsFloor2.activeSelf)
+        {
+            TinyWoodsFloor2.SetActive(false);
+            Bulbasaur.transform.position = new Vector3(0.73f, -0.65f, 1f);
+            BossFloor.SetActive(true);
+        }
     }
 
 
     public void RestartGame()
     {
         //ResetTimeScale();
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("GameScene");
     }
 }
